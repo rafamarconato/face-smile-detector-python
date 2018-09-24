dad2038387f60ac70f4c20d4f55fbfe2d4f93bcd
Rafaela Marconato <raffamarconato@gmail.com>
2018-09-23 21:28:37 -0300

    Aumenta a espessura da borda

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0))
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 5)
     face_gray = gray[y:y + h, x:x + w]
     face_color = img[y:y + h, x:x + w]
     smiles = smile_cascade.detectMultiScale(face_gray, 1.5, 20)
     for (sx, sy, sw, sh) in smiles:
-        cv2.rectangle(face_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255))
+        cv2.rectangle(face_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255), 5)
 
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.grid(False)
